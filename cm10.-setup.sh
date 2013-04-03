ec18d40d0dbf522d51474fbd92b3812edb158a71
brandon15811 <brandon15811@gmail.com>
2013-04-03 00:21:37 -0500

    Create cm10.-setup.sh

---
+++ cm10.-setup.sh
@@ -0,0 +1,23 @@
+WORKSPACE=$1
+REPO_BRANCH=$2
+
+echo "Exec'ing vendor/cm/get-prebuilts"
+vendor/cm/get-prebuilts
+
+if [ ! -z "$PATCH" ]
+then
+  $WORKSPACE/hudson/applypatch.py "$PATCH"
+fi
+if [ ! -z "$CUSTOMPATCH1PATH" ]
+then
+	cd $WORKSPACE/$REPO_BRANCH/$CUSTOMPATCH1PATH
+	echo "Applying Custom Patch 1 to "$CUSTOMPATCH1PATH
+	git apply $WORKSPACE/custompatch1.patch
+fi
+
+if [ ! -z "$CUSTOMPATCH2PATH" ]
+then
+	cd $WORKSPACE/$REPO_BRANCH/$CUSTOMPATCH2PATH
+	echo "Applying Custom Patch 2 to "$CUSTOMPATCH2PATH
+	git apply $WORKSPACE/custompatch2.patch
+fi
